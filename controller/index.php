c38b983fd3ea16df0713a270eae496e259b48027
Doge <clement.kubec@isep.fr>
2015-12-04 00:25:23 +0100

    Avec des vrais morceaux de class dedans

--- controller/index.php
+++ controller/index.php
@@ -1,15 +1,35 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
+<?php
+echo 'Bienvenue sur NDLI2015';
+
+$page = $_GET['page'];
+
+
+
+
+if ($page == 'article') {
+
+	echo "article";
+
+} elseif ($page == 'home') {
+
+	echo "home";
+
+} elseif ($page == 'faq') {
+
+	
+
+} elseif ($page == 'ajout-disaster') {
+
+	include "/../views/disaster/ajout-disaster.html";
+
+}
+
+
+
+
+
+
+
+
+
+?>
