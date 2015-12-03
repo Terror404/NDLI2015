71ce2c3294cfdbc79990209918203df776dd4562
AnastasiaProf <anastasia.chinsky@isep.fr>
2015-12-03 23:04:09 +0100

    modify homepage

--- views/skeleton.php
+++ views/skeleton.php
@@ -22,82 +22,18 @@
 	</div>
 </header>
 
-<nav class="navbar navbar-default">
-	<div class="col-xs-12 col-md-12" style="background-color:yellow;">
-		<?php include("nav_bar.html"); ?>
+<nav class="navbar-default">
+	<div class="row">
+		<div class="col-xs-12 col-md-12" style="background-color:yellow;">
+			<?php include("nav_bar.html"); ?>
+		</div>
 	</div>
 </nav>
 
 <main>
 
-	<style type="text/css"> 
-body {
-  font-family:'Helvetica Neue', Arial, sans-serif;
-}
-
-.item {
-  width: 200px;
-  height: 200px;
-  margin: 3px;
-  float: left;
-  
-  
-}
-.item.next {
-    width: 60px;
-    height: 60px;
-    border: 5px solid black;
-    border-radius: 35px;
-}
-.red {
-    background: red;
-}
-.blue {
-    background: blue;
-}
-.green {
-    background: green;
-}
-.yellow {
-    background: yellow;
-}
-/* Start: Recommended Isotope styles */
-
-/**** Isotope Filtering ****/
-.isotope-item {
-    z-index: 2;
-}
-.isotope-hidden.isotope-item {
-    pointer-events: none;
-    z-index: 1;
-}
-/**** Isotope CSS3 transitions ****/
- .isotope, .isotope .isotope-item {
-    -webkit-transition-duration: 0.8s;
-    -moz-transition-duration: 0.8s;
-    transition-duration: 0.8s;
-}
-.isotope {
-    -webkit-transition-property: height, width;
-    -moz-transition-property: height, width;
-    transition-property: height, width;
-}
-.isotope .isotope-item {
-    -webkit-transition-property: -webkit-transform, opacity;
-    -moz-transition-property: -moz-transform, opacity;
-    transition-property: transform, opacity;
-}
-/**** disabling Isotope CSS3 transitions ****/
- .isotope.no-transition, .isotope.no-transition .isotope-item, .isotope .isotope-item.no-transition {
-    -webkit-transition-duration: 0s;
-    -moz-transition-duration: 0s;
-    transition-duration: 0s;
-}
-/* End: Recommended Isotope styles */
-
-	</style>
 <div class="row">
-  <div class="col-sm-12 col-md-10 col-md-offset-1">
+  <div class="col-sm-12 col-md-12">
   	<div id="container">
 	    <div class="item red"></div>
 	    <div class="item blue"></div>
@@ -129,7 +65,10 @@ body {
 
     $container.isotope({
         itemSelector: '.item'
+
     });
+
+
 </script>
 
 
