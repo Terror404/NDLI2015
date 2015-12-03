b3365a0292a6df4b61f7b7efae1d58969a82b8d6
PierreGrincheux <pierre.prunieres@live.fr>
2015-12-04 00:32:30 +0100

    another commit

---
+++ views/backoffice/user_page.php
@@ -0,0 +1,17 @@
+<p> Bienvenue machin truc</p>
+
+<div>	
+<a href="ce super liens vers le formulaire de création">Ajouter type de catastrophe</a>
+</div>
+
+<div>	
+<a href="ce super liens vers le formulaire de création">Ajouter un article</a>
+</div>
+
+<div>	
+<a href="ce super liens vers le formulaire de création">Modifier / Supprimer des articles existants</a>
+</div>
+		
+
+
+
